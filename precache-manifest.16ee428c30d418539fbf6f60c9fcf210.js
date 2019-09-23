43301831499a4b5b1a829e41060f47c785ffba9a
sylvestrevgen <sylvestrevgen@gmail.com>
2019-09-23 18:37:56 +0300

    Updates

---
+++ precache-manifest.16ee428c30d418539fbf6f60c9fcf210.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "459f2f9fd249b2609632a0814cebd3a6",
+    "url": "/goit-react-hw-03-bank-account/index.html"
+  },
+  {
+    "revision": "5ee157d75bf97a1f4774",
+    "url": "/goit-react-hw-03-bank-account/static/css/2.39017165.chunk.css"
+  },
+  {
+    "revision": "64f0aff36491e7fa4c63",
+    "url": "/goit-react-hw-03-bank-account/static/css/main.463d5f9f.chunk.css"
+  },
+  {
+    "revision": "5ee157d75bf97a1f4774",
+    "url": "/goit-react-hw-03-bank-account/static/js/2.f6a23bdc.chunk.js"
+  },
+  {
+    "revision": "64f0aff36491e7fa4c63",
+    "url": "/goit-react-hw-03-bank-account/static/js/main.4796f438.chunk.js"
+  },
+  {
+    "revision": "5ae27dd2b4af87281072",
+    "url": "/goit-react-hw-03-bank-account/static/js/runtime-main.86f7a662.js"
+  }
+]);
